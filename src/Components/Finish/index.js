6a96cbdecf796c7c1a85d9e76c2b3416c3f27482
Javier Pyke <pyke.jn@gmail.com>
2021-05-05 15:42:37 +0200

    act

--- src/Components/Finish/index.js
+++ src/Components/Finish/index.js
@@ -4,7 +4,6 @@ export default function Finish(props){
     return(
         <div className='finish'>
             <span>¡El juego ha terminado!</span>
-            gdfdgdfgdfgdfgdfgdf
             <span style={{color:'#4FD1C5', fontSize:'90px'}}>{props.score}</span><span style={{fontSize:'20px'}}> pts.</span>
         </div>
     )

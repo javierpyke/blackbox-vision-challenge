111deba6e705aeae06b90ca839fe6116fb3e00b9
Javier Pyke <pyke.jn@gmail.com>
2021-05-06 15:52:40 -0300

    act

--- src/Components/Finish/index.js
+++ src/Components/Finish/index.js
@@ -9,7 +9,8 @@ export default function Finish(props){
         backgroundColor= 'rgb(255, 255, 255)'
         borderRadius= '10px'
         boxShadow= '0px 3px 12px rgba(0, 0, 0, 0.25)'
-        color= 'black'> 
+        color= 'black'
+        textAlign='center'> 
             {props.children}
             <span style={{color:'#4FD1C5', fontSize:'90px'}}>{props.score}</span><span style={{fontSize:'20px'}}> pts.</span>
         </Box>
